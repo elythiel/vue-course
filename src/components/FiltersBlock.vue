a3fe9009a8290a45e46aa53147a555fb60bff735
Mickael Maillot <mickael.maillot@ageval.fr>
2023-11-27 17:41:40 +0100

    4 - Bloc de filtres

---
+++ src/components/FiltersBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div aria-describedby="filters-title" class="bg-white rounded mb-6 text-neutral-900 py-2 px-4">
+    <h2 id="filters-title" class="text-xl">Filtrer les cartes</h2>
+    <label>
+      Afficher uniquement les légendaires
+      <input type="checkbox"
+             :value="isLegendary"
+             @change="(event) => $emit('update:isLegendary', event.target?.checked ?? false)"
+      />
+    </label>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  isLegendary: boolean;
+}>();
+
+defineEmits<{
+  'update:isLegendary': [isLegendary: boolean];
+}>()
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,10 @@
   <h1>
     Liste des cartes
   </h1>
+  <FiltersBlock v-model:is-legendary="filters.isLegendary" />
+
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-    <div v-for="card in cardsStore.cards ?? []"
+    <div v-for="card in filtererdCards"
          :key="card.name"
          class="flex flex-col gap-2 bg-white rounded text-neutral-800 p-4"
     >
@@ -37,9 +39,22 @@
 <script setup lang="ts">
 import CardImage from "@/components/CardImage.vue";
 import {useCardsStore} from "@/stores/cards";
-import {onBeforeMount} from "vue";
+import {computed, onBeforeMount, ref} from "vue";
+import FiltersBlock from "@/components/FiltersBlock.vue";
 
 const cardsStore = useCardsStore();
+
+const filters = ref({
+  isLegendary: false
+});
+
+const filtererdCards = computed(() => {
+  if (filters.value.isLegendary === false) {
+    return cardsStore.cards;
+  }
+
+  return cardsStore.cards.filter((card) => card.isLegendary);
+})
 
 onBeforeMount(() => {
   if (cardsStore.cards.length === 0) {

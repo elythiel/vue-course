9416d391f8cfbfd93da5251da4e606de6c9984f9
Mickael Maillot <mickael.maillot@ageval.fr>
2023-11-27 16:35:19 +0100

    1 - Création de la page home

---
+++ src/components/CardImage.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    type="button"
+    class="py-1 px-2 rounded bg-sky-900 text-white"
+    @click="isOpened = !isOpened"
+  >
+    Voir la carte
+  </button>
+  <template v-if="isOpened">
+    <Teleport to="body">
+      <div
+        class="fixed top-0 left-0 h-screen w-screen bg-black/10 flex justify-center items-center"
+        aria-live="polite"
+      >
+        <button
+          type="button"
+          class="absolute top-4 right-4 bg-stone-300 text-black rounded-full text-3xl h-16 w-16"
+          title="Fermer l'aperçu"
+          @click="isOpened = false"
+        >
+          X
+        </button>
+        <img
+          :src="`/images/${imageSrc}`"
+          :alt="`Carte : ${cardName}`"
+          class="max-h-[75%]"
+        />
+      </div>
+    </Teleport>
+  </template>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+
+defineProps<{
+  cardName: string;
+  imageSrc: string|File;
+}>();
+
+const isOpened = ref(false);
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>Page d'accueil</h1>
+  <h1 class="text-2xl mb-6">
+    Liste des cartes
+  </h1>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div v-for="card in cards ?? []" :key="card.name" class="flex flex-col gap-2 bg-white rounded text-neutral-800 p-4">
+      <h2 class="text-lg">
+        {{ card.name }}
+      </h2>
+      <p>{{ card.types }}</p>
+      <h3 class="font-bold">Coût :</h3>
+      <ul>
+        <li v-for="(cost, key) in card.cost" :key="key">
+          {{ key }} : {{ cost }}
+        </li>
+      </ul>
+      <p>
+        {{ card.description }}
+      </p>
+      <p>
+        Attaque : {{ card.attack }} - Défense :  {{ card.defense }}
+      </p>
+      <div class="mt-auto">
+        <CardImage :card-name="card.name" :image-src="card.image" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import {useDatas} from "@/composables/datas";
+import CardImage from "@/components/CardImage.vue";
 
 const cards = useDatas();
 </script>
